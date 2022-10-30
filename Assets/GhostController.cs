25b60db710c9dc5289d10e96feb500321f81918c
Brandon Cramer <brandon.cramer1997@gmail.com>
2022-10-30 15:35:30 +1100

    Create AI Template Class

--- Assets/GhostController.cs
+++ Assets/GhostController.cs
@@ -2,16 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Behaviour
+{
+    One, Two, Three, Four
+}
+
+public enum GhostState
+{
+    Moving,
+    Dead,
+}
+
 public class GhostController : MonoBehaviour
 {
+    public Tweener tweener;
+    public LevelGenerator levelGenerator;
+    public List<Vector2> Center;
+
+    public Behaviour ghostBehaviour;
+    private Behaviour defaultBehaviour;
+
+    private Movement lastInput = Movement.MoveLeft;
+    private Movement currentInput = Movement.MoveLeft;
+
     private float DeadInterval = 5f;
     private float DeadTimer = 0f;
 
     private AnimationStateController animStateController;
 
+
+
     private void Start()
     {
         animStateController = GetComponent<AnimationStateController>();
+        defaultBehaviour = ghostBehaviour;
     }
 
     // Update is called once per frame
@@ -36,6 +60,51 @@ public class GhostController : MonoBehaviour
                 SetToWalk();
             }
         }
+    }
+
+    private void ComputeInput()
+    {
+        if (ghostBehaviour == Behaviour.One)
+        {
+            lastInput = Movement.MoveUp;
+        }
+        else if (ghostBehaviour == Behaviour.Two)
+        {
+            lastInput = Movement.MoveLeft;
+        }
+        else if (ghostBehaviour == Behaviour.Three)
+        {
+            lastInput = Movement.MoveDown;
+        }
+        else if (ghostBehaviour == Behaviour.Four)
+        {
+            lastInput = Movement.MoveRight;
+        }
+    }
+
+    private void Behaviour1()
+    {
+
+    }
+
+    private void Behaviour2()
+    {
+
+    }
+
+    private void Behaviour3()
+    {
+
+    }
+
+    private void Behaviour4()
+    {
+
+    }
+
+    private void TweenToCenter()
+    {
+
     }
 
     public void RunDeadTimer()
